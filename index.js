bf9e50f87a05138fb66dd2d63b4ed9dcf4c283b9
DESKTOP-H8CRQBK\xulid <Xulidinh88@gmail.com>
2021-12-21 23:15:48 +0700

    Push

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || "3000";
 const url = require("url");
